17f99ff8ae84e6f65f0f921c74bc9a20cc7e76be
Jeroen Diel <jeroen@nerdbox.it>
2018-11-26 15:59:13 +0100

    Create portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,15 @@
+from socket import * 
+
+if __name__ == '__main__':
+    targetIP = raw_input('Enter host to scan: ')
+    print 'Starting scan on host ', targetIP
+
+    #scan reserved ports
+    for i in range(20, 1025):
+        s = socket(AF_INET, SOCK_STREAM)
+
+        result = s.connect_ex((targetIP, i))
+
+        if(result == 0) :
+            print 'Port %d: OPEN' % (i,)
+        s.close()
